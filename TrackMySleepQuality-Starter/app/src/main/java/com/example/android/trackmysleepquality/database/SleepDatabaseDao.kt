d856c1380307d086117e1af5c7b5816514726026
Thiago Pedron <thiagopedron70@gmail.com>
2022-12-07 09:28:23 -0300

    atividade finalizada 07-1222

--- TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -19,4 +19,17 @@ package com.example.android.trackmysleepquality.database
 import androidx.room.Dao
 
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao{
+    @Insert
+    fun insert(night: SleepNight)
+    @Update
+    fun update(night: SleepNight)
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+}
